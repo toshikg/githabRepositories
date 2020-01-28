f7f5dc98d1e655f4a2640cda3eb402624c93986c
Anton Goncharov <toshikgonch@gmail.com>
2020-01-28 18:26:31 +0200

    fetch repo details

--- src/app/repos/repo-details/repo-details.component.ts
+++ src/app/repos/repo-details/repo-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ReposService} from '../repos.service';
+import {ActivatedRoute} from '@angular/router';
+import {Observable} from 'rxjs';
+import {Repo} from '../repos/repos';
 
 @Component({
   selector: 'app-repo-details',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repo-details.component.scss']
 })
 export class RepoDetailsComponent implements OnInit {
+  private repo: Observable<Repo>;
 
-  constructor() { }
+  constructor(
+    private reposService: ReposService,
+    private route: ActivatedRoute
+    ) {
+
+  }
 
   ngOnInit() {
+    const repoId = parseInt(this.route.snapshot.params.id, 0);
+    this.repo = this.reposService.getRepoById(repoId);
   }
 
 }
--- src/app/repos/repos.service.ts
+++ src/app/repos/repos.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import {environment} from '../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {Repo} from './repos/repos';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
+import {tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,40 @@ import {Observable} from 'rxjs';
 export class ReposService {
 
   private api = environment.gitHabUrl;
-  constructor(private http: HttpClient) {}
+  private repos: Repo[] = [];
 
-  getRepos(): Observable<Repo[]> {
+  constructor(private http: HttpClient) {
+  }
+
+  private fetchRepos(): Observable<Repo[]> {
     const url = `${this.api}/users/toshikg/repos`;
 
     return this.http.get<Repo[]>(url);
+  }
+
+  getRepos(): Observable<Repo[]> {
+    if (this.repos.length === 0) {
+      return this.fetchRepos().pipe(
+        tap(repos => this.repos = repos)
+      );
+    } else {
+      return of(this.repos);
+    }
+  }
+
+  private fetchRepo(id: number): Observable<Repo> {
+    const url = `${this.api}/repositories/${id}`;
+
+    return this.http.get<Repo>(url);
+  }
+
+  getRepoById(id: number): Observable<Repo> {
+    const repoCached = this.repos.find(r => r.id === id);
+
+    if (repoCached) {
+      return of(repoCached);
+    } else {
+      return this.fetchRepo(id);
+    }
   }
 }
