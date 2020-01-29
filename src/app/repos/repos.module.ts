afbf3102b2130e838192d33467e874c28b506b82
Anton Goncharov <toshikgonch@gmail.com>
2020-01-29 10:42:41 +0200

    add filtering and sorting

--- src/app/repos/repos.module.ts
+++ src/app/repos/repos.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReposComponent } from './repos/repos.component';
 import { RepoDetailsComponent } from './repo-details/repo-details.component';
-import {MatFormFieldModule, MatInputModule, MatListModule} from '@angular/material';
+import {MatFormFieldModule, MatInputModule, MatListModule, MatSelectModule} from '@angular/material';
 import {RouterModule} from '@angular/router';
 import {ReactiveFormsModule} from '@angular/forms';
+import {FlexModule} from '@angular/flex-layout';
 
 
 
@@ -16,7 +17,9 @@ import {ReactiveFormsModule} from '@angular/forms';
     RouterModule,
     MatFormFieldModule,
     MatInputModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FlexModule,
+    MatSelectModule
   ]
 })
 export class ReposModule { }
--- src/app/repos/repos/repos.component.ts
+++ src/app/repos/repos/repos.component.ts
@@ -4,6 +4,7 @@ import {Repo} from './repos';
 import {combineLatest, Observable} from 'rxjs';
 import {FormControl} from '@angular/forms';
 import {map, startWith} from 'rxjs/operators';
+import * as _orderBy from 'lodash/orderBy';
 
 @Component({
   selector: 'app-repos',
@@ -12,21 +13,37 @@ import {map, startWith} from 'rxjs/operators';
 })
 export class ReposComponent implements OnInit {
   repos: Observable<Repo[]>;
-  private filter: FormControl;
+  private filterCtrl: FormControl;
   private reposFiltered: Observable<Repo[]>;
+  sorting: string[];
+  sortNames = {
+    DATE_ASC: 'Date ASC',
+    DATE_DESC: 'Date DESC',
+    TITLE_ASC: 'Title ASC',
+    TITLE_DESC: 'Title DESC',
+  };
+  private sortingCtrl: FormControl;
 
   constructor(reposService: ReposService) {
     this.repos = reposService.getRepos();
-    this.filter = new FormControl('');
+    this.filterCtrl = new FormControl('');
+    this.sortingCtrl = new FormControl('');
 
     this.reposFiltered = combineLatest([
       this.repos,
-      this.filter.valueChanges.pipe(startWith(''))
+      this.filterCtrl.valueChanges.pipe(startWith('')),
+      this.sortingCtrl.valueChanges.pipe(startWith(''))
     ]).pipe(
-      map(([repos, searchStr]) => {
-        return repos.filter(h => h.name.toLowerCase().includes(searchStr.toLowerCase()));
+      map(([repos, searchStr, sort]) => {
+        const filteredRepos = repos.filter(h => h.name.toLowerCase().includes(searchStr.toLowerCase()));
+        const sortField = sort.startsWith('DATE') ? 'updated_at' : 'name';
+        const sortDirection = sort.indexOf('ASC') > -1 ? 'asc' : 'desc';
+
+        return _orderBy(filteredRepos, [sortField], [sortDirection]);
       })
     );
+
+    this.sorting = ['DATE_ASC', 'DATE_DESC', 'TITLE_ASC', 'TITLE_DESC'];
   }
 
   ngOnInit() {
