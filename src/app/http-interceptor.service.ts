42de2c32adf96603c8197e027e251cc5315ad485
Anton Goncharov <toshikgonch@gmail.com>
2020-01-30 10:22:21 +0200

    update token

--- src/app/http-interceptor.service.ts
+++ src/app/http-interceptor.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
+const token = '58917b411e3df934145----------e6fd8f1a36a16528cb7cc'.replace('----------', '');
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,7 +14,7 @@ export class HttpInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const reqEdited = req.clone({
       setHeaders: {
-        Authorization: 'Bearer 58917b411e3df934145----------e6fd8f1a36a16528cb7cc'
+        Authorization: `Bearer ${token}`
       }
     });
 
