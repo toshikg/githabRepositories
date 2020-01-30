3a8bd79463e9be86878d755843063992ef97ffcc
Anton Goncharov <toshikgonch@gmail.com>
2020-01-30 10:19:52 +0200

    update token

--- src/app/http-interceptor.service.ts
+++ src/app/http-interceptor.service.ts
@@ -12,7 +12,7 @@ export class HttpInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const reqEdited = req.clone({
       setHeaders: {
-        Authorization: 'Bearer be879f1d8b5e89f636e756bd23403cca0adc7aab'
+        Authorization: 'Bearer 58917b411e3df934145----------e6fd8f1a36a16528cb7cc'
       }
     });
 
