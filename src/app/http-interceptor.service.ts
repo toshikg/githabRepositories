4f2b8eddbefeb5450fb77661145d06184ad0c4dd
toshikg <toshikgonch@gmail.com>
2020-01-30 10:30:28 +0200

    Update http-interceptor.service.ts

--- src/app/http-interceptor.service.ts
+++ src/app/http-interceptor.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
-// this need to get around githab security. Github not allows token in source code
+// this need to get around githab security. Github not allows token in source code.
 const token = '58917b411e3df934145----------e6fd8f1a36a16528cb7cc'.replace('----------', '');
 
 @Injectable({
